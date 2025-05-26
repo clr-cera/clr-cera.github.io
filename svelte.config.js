7f122b89dcbcde42eb62f5678e7b616ac8ca0bdd
clr <clrcera05@gmail.com>
2025-05-26 10:16:26 -0300

    formatting

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: [vitePreprocess()],
-  kit: { adapter: adapter({ fallback: '404.html' }) },
-  extensions: ['.svelte', '.svx'],
-  paths: {
-    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-  }
+	preprocess: [vitePreprocess()],
+	kit: { adapter: adapter({ fallback: '404.html' }) },
+	extensions: ['.svelte', '.svx'],
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+	}
 };
 
 export default config;
