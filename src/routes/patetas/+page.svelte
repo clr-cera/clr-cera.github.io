f1098d6f1e19e20e5d66f5849948f1a1f7802078
clr <clrcera05@gmail.com>
2025-06-15 20:27:35 -0300

    patetascerto

---
+++ src/routes/patetas/+page.svelte
@@ -0,0 +1,12 @@
+<div class="m-2 flex flex-col items-center justify-center py-1 text-xl font-bold">
+	<h1 class="text-mypurple">Ingredients</h1>
+	<ul>
+		<li>1 big egg or 2 small eggs</li>
+		<li>1 cup of milk</li>
+		<li>1/2 tbsp of melted butter</li>
+		<li>2 tbsp of sugar</li>
+		<li>1 cup of flour</li>
+	</ul>
+	<h1 class="text-mypurple mt-4">Procedure</h1>
+	Blend everything
+</div>
