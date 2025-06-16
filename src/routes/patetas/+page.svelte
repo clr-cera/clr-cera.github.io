fcb37260ceb5ac08803e582041f181f51c4d1a04
clr <clrcera05@gmail.com>
2025-06-16 13:32:48 -0300

    feat/End patetas recipe

--- src/routes/patetas/+page.svelte
+++ src/routes/patetas/+page.svelte
@@ -1,5 +1,13 @@
 <div class="m-2 flex flex-col items-center justify-center py-1 text-xl font-bold">
-	<h1 class="text-mypurple">Ingredients</h1>
+	<h1 class="text-mypurple">Thanks Grandma</h1>
+	<div class="text-center">
+		<p>
+			My grandma teached me this recipe,<br />she does for me and my cousins every time we get to
+			her house,<br />and I will always have it in my heart<br />
+		</p>
+	</div>
+
+	<h1 class="text-mypurple mt-4">Ingredients</h1>
 	<ul>
 		<li>1 big egg or 2 small eggs</li>
 		<li>1 cup of milk</li>
@@ -7,6 +15,14 @@
 		<li>2 tbsp of sugar</li>
 		<li>1 cup of flour</li>
 	</ul>
+
 	<h1 class="text-mypurple mt-4">Procedure</h1>
-	Blend everything
+	<ul class="text-center">
+		<li>Blend everything</li>
+		<li>Use the pan that was used to melt the butter to fry each patetas</li>
+		<li>Put a thin layer</li>
+		<li>Fry until the upper part is not liquid anymore and flip it</li>
+		<li>Keep checking the bottom part, when it shows brown circles you can remove it</li>
+		<li>Enjoy! 😋</li>
+	</ul>
 </div>
