0a2140eb2491cb4663d0b3bd207d1812b58ae040
clr <clrcera05@gmail.com>
2025-06-17 08:34:15 -0300

    add about text and fix navbar on large screens

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -14,7 +14,7 @@
 			alt="Logo"
 		/>
 	</a>
-	<div class="text-myorange flex flex-row items-center justify-center p-4 text-xl md:text-base">
+	<div class="text-myorange flex flex-row items-center justify-center p-4 text-xl">
 		<a class="px-3" href="/"><strong>Home</strong></a>
 		<a class="px-3" href="/about"><strong>About</strong></a>
 		<a class="px-3" href="/blog"><strong>Blog</strong></a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<div class="">
+	<h1 class="text-mypurple text-center text-2xl font-bold">Clara</h1>
+	<div class="mt-3 flex flex-col content-center justify-center text-center text-xl text-wrap">
+		<h1 class="text-mypurple text-2xl">Hi!👋</h1>
+		<p class="mt-3">
+			My name is Clara but you can call me clr!<br />
+			I was born in 2005, in Brasilia, Brazil and<br />
+			currently a Computer Science student at the University of São Paulo (USP)<br />
+		</p>
+	</div>
+</div>
