a58845645a351f43e87ddab781a39e62e2c52097
clr <clrcera05@gmail.com>
2025-07-05 11:46:24 -0300

    fix/name

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,4 @@
 <div class="text-myorange m-2 flex flex-col items-center justify-center py-1 text-xl font-bold">
 	<a class="my-2" href="https://element.clr.dev.br"> Element Store </a>
-	<a class="my-2" href="https://cafelattes.clr.dev.br"> Element Store </a>
+	<a class="my-2" href="https://cafelattes.clr.dev.br"> Cafe Lattes </a>
 </div>
