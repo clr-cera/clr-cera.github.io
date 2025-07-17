da3a2e2cae84e4cbfd226414d413c5d4c0d8b436
clr <clrcera05@gmail.com>
2025-07-17 16:43:16 -0300

    fix/nav

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,25 +1,64 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const collapseMenu = document.getElementById('MenuLines');
+		const collapseBtn = document.getElementById('menubtn');
+
+		if (!collapseMenu || !collapseBtn) {
+			console.error('Menu elements not found');
+			return;
+		}
+
+		collapseBtn.addEventListener('click', () => {
+			collapseMenu.classList.toggle('collapse');
+		});
+	});
+</script>
+
 <nav
-	class="border-mypurple flex flex-row
-  items-center justify-center
-  border-b-1
-  px-2 py-1.5
-  text-white
+	class="border-mypurple flex flex-col items-center
+  justify-center border-b-1
+  px-2
+  py-1.5 text-white
+  md:flex-row
   "
 >
-	<a href="https://github.com/clr-cera">
-		<img
-			class="max-width-[100px] mx-2 my-1 max-h-[50px] rounded-xl"
-			id="footerlogo"
-			src="https://avatars.githubusercontent.com/u/93736542?v=4"
-			alt="Logo"
-		/>
-	</a>
-	<div class="text-myorange flex flex-row items-center justify-center p-4 text-xl">
-		<a class="px-3" href="/"><strong>Home</strong></a>
-		<a class="px-3" href="/about"><strong>About</strong></a>
-		<a class="px-3" href="/blog"><strong>Blog</strong></a>
-		<a class="px-3" href="/contact"><strong>Contact</strong></a>
-		<a class="px-3" href="/patetas"><strong>Patetas</strong></a>
-		<a class="px-3" href="/projects"><strong>Projects</strong></a>
+	<div class="flex flex-row">
+		<a href="https://github.com/clr-cera">
+			<img
+				class="max-width-[100px] mx-2 my-1 max-h-[50px] rounded-xl"
+				id="footerlogo"
+				src="https://avatars.githubusercontent.com/u/93736542?v=4"
+				alt="Logo"
+			/>
+		</a>
+		<button id="menubtn" class="collapse-btn text-myorange md:hidden" aria-label="Menu">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+				/>
+			</svg>
+		</button>
+	</div>
+	<div
+		id="MenuLines"
+		class="text-myorange collapse flex flex-col items-center justify-center p-4 text-xl *:px-3 *:py-2 md:visible md:flex-row"
+	>
+		<a href="/"><strong>Home</strong></a>
+		<a href="/about"><strong>About</strong></a>
+		<a href="/blog"><strong>Blog</strong></a>
+		<a href="/contact"><strong>Contact</strong></a>
+		<a href="/patetas"><strong>Patetas</strong></a>
+		<a href="/projects"><strong>Projects</strong></a>
 	</div>
 </nav>
