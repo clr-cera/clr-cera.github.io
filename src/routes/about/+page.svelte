8f132f99b8c1dc434a201ec79806c96b28494077
clr <clrcera05@gmail.com>
2025-07-17 17:09:28 -0300

    change about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,12 @@
 <div class="">
 	<h1 class="text-mypurple text-center text-2xl font-bold">Clara</h1>
-	<div class="mt-3 flex flex-col content-center justify-center text-center text-xl text-wrap">
-		<h1 class="text-mypurple text-2xl">Hi!👋</h1>
-		<p class="mt-3">
-			My name is Clara but you can call me clr!<br />
-			I was born in 2005, in Brasilia, Brazil and<br />
-			currently a Computer Science student at the University of São Paulo (USP)<br />
-		</p>
+	<div
+		class="mt-2 flex flex-col content-center items-center justify-center text-center text-xl text-wrap"
+	>
+		<ul class="mt-3 flex list-disc flex-col items-center gap-1 px-4">
+			<li>I am 20y/o</li>
+			<li>Currently a Computer Science student at the University of São Paulo (USP)</li>
+			<li>Developing research on CSAI detection at the Araceli Research Group</li>
+		</ul>
 	</div>
 </div>
